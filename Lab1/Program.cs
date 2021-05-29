d7be343c1c4489acf3002a98ef84ffb7f5fef31e
njk nkl <baev-lev1@mail.ru>
2021-05-29 23:20:15 +0700

    7 fixed

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -16,16 +16,20 @@ namespace Lab1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ParentForm());
-            Application.Run(new WinQuestioncs());
-            Application.Run(new TestList());
-            Application.Run(new WinContainer());
-            Application.Run(new WinLinkLabel());
-            Application.Run(new RegistrationForm());
-            Application.Run(new TestStandartDialog());
-            Application.Run(new PDF_Reader());
-            Application.Run(new WinAnim());
-            Application.Run(new WinPrint());
+            /*  Application.Run(new ParentForm());
+              Application.Run(new WinQuestioncs());
+              Application.Run(new TestList());
+              Application.Run(new WinContainer());
+              Application.Run(new WinLinkLabel());
+              Application.Run(new RegistrationForm());
+              Application.Run(new TestStandartDialog());
+              Application.Run(new PDF_Reader());
+              Application.Run(new WinAnim());
+              Application.Run(new WinPrint());*/
+            Application.Run(new WinBackgroungWorker());
+            Application.Run(new WinAsynchDelegate());
+            Application.Run(new WinAsynchMethod());
+            Application.Run(new WinLanguage());
         }
     }
 }
---
+++ Lab1/WinAsynchMethod.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class WinAsynchMethod : Form
+    {
+        public WinAsynchMethod()
+        {
+            InitializeComponent();
+        }
+
+        private delegate int AsyncSumm(int a, int b);
+
+
+
+        private int Summ(int a, int b)
+        {
+            System.Threading.Thread.Sleep(9000);
+            return a + b;
+        }
+
+
+        private void WynAsynchMethod_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CallBackMethod(IAsyncResult ar)
+        {
+            string str;
+            AsyncSumm summdelegate = (AsyncSumm)ar.AsyncState;
+            str = String.Format("Сумма введенных чисел равна {0}", summdelegate.EndInvoke(ar));
+            MessageBox.Show(str, "Результат операции");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            try
+            {
+                a = Int32.Parse(txtbA.Text);
+                b = Int32.Parse(txtbB.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("при выполнении преобразования типов возникла ошибка");
+                txtbA.Text = txtbB.Text = "";
+                return;
+            }
+            AsyncSumm summdelegate = new AsyncSumm(Summ);
+            AsyncCallback cb = new AsyncCallback(CallBackMethod);
+            summdelegate.BeginInvoke(a, b, cb, summdelegate);
+        }
+
+        private void btnWork_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работа кипит!");
+        }
+
+        private void Help_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Help.ShowHelp(this, helpProvider1.HelpNamespace);
+        }
+    }
+}
